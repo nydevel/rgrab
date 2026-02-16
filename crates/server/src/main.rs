4b52a4bc56a2841b79e9d0ad9601206cdf296b05
nydevel <1@shifu.io>
2026-02-16 13:23:10 +0500

    in rpgress

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -2,11 +2,7 @@ mod config;
 
 use anyhow::Result;
 use axum::Router;
-use hyper::body::Incoming;
-use hyper_util::rt::{TokioExecutor, TokioIo};
-use hyper_util::server::conn::auto::Builder;
 use storage::rocks_store::RocksStore;
-use tower::Service;
 use tower_http::cors::CorsLayer;
 use tracing_subscriber::EnvFilter;
 
@@ -31,24 +27,7 @@ async fn main() -> Result<()> {
 
     tracing::info!("Server listening on {}", cfg.listen);
     let listener = tokio::net::TcpListener::bind(&cfg.listen).await?;
+    axum::serve(listener, app).await?;
 
-    loop {
-        let (stream, _addr) = listener.accept().await?;
-        let tower_service = app.clone();
-
-        tokio::spawn(async move {
-            let hyper_service = hyper::service::service_fn(move |req: hyper::Request<Incoming>| {
-                let mut svc = tower_service.clone();
-                async move { svc.call(req).await }
-            });
-
-            let builder = Builder::new(TokioExecutor::new());
-            if let Err(e) = builder
-                .serve_connection(TokioIo::new(stream), hyper_service)
-                .await
-            {
-                tracing::debug!("Connection error: {}", e);
-            }
-        });
-    }
+    Ok(())
 }
